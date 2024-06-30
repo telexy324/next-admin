0e50fa3bedcc3a0d475d943a213d4a08705637de
Yang Xu <yangxu@YangdeMacBook-Pro.local>
2024-07-01 06:21:13 +0800

    202040701

--- src/app/[locale]/api/chart/route.ts
+++ src/app/[locale]/api/chart/route.ts
@@ -1,72 +1,107 @@
 import {  NextRequest } from 'next/server'
 import { sleep } from '@/utils';
+import {db} from "@/utils/db";
 
-const boardList = [
-    {
-        id: '1',
-        w: 'calc(30% - 16px)',
-        h: 320,
-        type: 'bubble-bar',
-        data: {
-            type: 'static',
-            value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
-        }
-    },
-    {
-        id: '2',
-        w: 'calc(34% - 16px)',
-        h: 320,
-        type: 'select-bar',
-        data: {
-            type: 'static',
-            value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
-        }
-    },
-    {
-        id: '3',
-        w: 'calc(36% - 16px)',
-        h: 320,
-        type: 'insert-bar',
-        data: {
-            type: 'static',
-            value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
-        }
-    },
+// const boardList = [
+//     {
+//         id: '1',
+//         w: 'calc(30% - 16px)',
+//         h: 320,
+//         type: 'bubble-bar',
+//         data: {
+//             type: 'static',
+//             value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
+//         }
+//     },
+//     {
+//         id: '2',
+//         w: 'calc(34% - 16px)',
+//         h: 320,
+//         type: 'select-bar',
+//         data: {
+//             type: 'static',
+//             value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
+//         }
+//     },
+//     {
+//         id: '3',
+//         w: 'calc(36% - 16px)',
+//         h: 320,
+//         type: 'insert-bar',
+//         data: {
+//             type: 'static',
+//             value: [43, 2, 5, 24, 53, 78, 82, 63, 49, 6]
+//         }
+//     },
+//
+//     {
+//         id: '4',
+//         w: 'calc(42% - 16px)',
+//         h: 320,
+//         type: 'timing',
+//         data: {
+//             type: 'fetch',
+//             value: 'https://gw.alipayobjects.com/os/bmw-prod/7fbb7084-cf34-4e7c-91b3-09e4748dc5e9.json',
+//         }
+//     },
+//     {
+//         id: '5',
+//         w: 'calc(58% - 16px)',
+//         h: 320,
+//         type: 'mutiFrame',
+//         data: {
+//             type: 'fetch',
+//             value: 'https://assets.antv.antgroup.com/g2/stocks2.json'
+//         }
+//     },
+// ]
 
-    {
-        id: '4',
-        w: 'calc(42% - 16px)',
-        h: 320,
-        type: 'timing',
-        data: {
-            type: 'fetch',
-            value: 'https://gw.alipayobjects.com/os/bmw-prod/7fbb7084-cf34-4e7c-91b3-09e4748dc5e9.json',
-        }
-    },
-    {
-        id: '5',
-        w: 'calc(58% - 16px)',
-        h: 320,
-        type: 'mutiFrame',
-        data: {
-            type: 'fetch',
-            value: 'https://assets.antv.antgroup.com/g2/stocks2.json'
-        }
-    },
-]
+interface DataItem {
+  type: string;
+  value?: string;
+  url?: string;
+};
+
+function isDataItem(data: any): data is DataItem {
+  return typeof data === 'object' && 'type' in data;
+}
 
 // 模拟用户获取数据
 export async function GET(req: NextRequest) {
     const plainData: any[] = [];
     const asyncData:any[] = [];
     const resultData: any[] = [];
-    boardList.forEach(v => {
-        if(v.data.type === 'fetch') {
-            asyncData.push(v);
-        }else {
-            plainData.push({...v, data: v.data.value});
+    const boardList = await db.board.findMany();
+    // if (boardList){
+    //   for (let b of boardList) {
+    //     if (b.data) {
+    //       const getData = b.data as DataItem
+    //       if(getData !== undefined && getData.type === 'fetch'  && getData.value) {
+    //         asyncData.push(b);
+    //       }else {
+    //         plainData.push({...b, data: getData.value});
+    //       }
+    //     }
+    //   }
+    //   // boardList.forEach(v => {
+    //   //   if(v.data && v.data.type === 'fetch') {
+    //   //     asyncData.push(v);
+    //   //   }else {
+    //   //     plainData.push({...v, data: v.data.value});
+    //   //   }
+    //   // });
+    // }
+    if (boardList) {
+      for (let b of boardList) {
+        if (b.data && isDataItem(b.data)) {
+          if (b.data.type === 'fetch' && b.data.value) {
+            asyncData.push(b);
+          } else {
+            plainData.push({ ...b, data: b.data.value });
+          }
         }
-    });
+      }
+    }
     asyncData.forEach(v => {
         fetch(v.data.value).then(res => res.json()).then(res => {
             resultData.push({...v, data: res})
